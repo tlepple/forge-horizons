2097d7a47d425e60744d58b4078ce5f476d8ec07
tlepple <49572881+tlepple@users.noreply.github.com>
2020-09-22 14:39:12 -0500

    Update aws_setup.sh

--- provider/aws/aws_setup.sh
+++ provider/aws/aws_setup.sh
@@ -2,6 +2,7 @@
 
 PROVIDER=$1
 TEMPLATE=$2
+BLOCKDEVICE=$3
 
 #########################################################
 # Input parameters
@@ -11,16 +12,17 @@ echo "Begin install into AWS Instance from the aws_setup.sh script..."
 
 echo "Parameter PROVIDER -->" $PROVIDER
 echo "Parameter TEMPLATE -->" $TEMPLATE
+echo "Parameter BLOCKDEICD -->" $BLOCKDEVICE
 ###########################################################################################################
 #time issues for clock offset in aws	
 ###########################################################################################################
 echo "setup clock offset issues for aws"
-#echo "server 169.254.169.123 prefer iburst minpoll 4 maxpoll 4" >> /etc/chrony.conf
-#systemctl restart chronyd
+echo "server 169.254.169.123 prefer iburst minpoll 4 maxpoll 4" >> /etc/chrony.conf
+systemctl restart chronyd
 
 
 ###########################################################################################################
 # install the common items from script 
 ###########################################################################################################
 echo "calling common_setup.sh here..."
-. $starting_dir/common/common_setup.sh $PROVIDER $TEMPLATE
+. $starting_dir/common/common_setup.sh $PROVIDER $TEMPLATE $BLOCKDEVICE
