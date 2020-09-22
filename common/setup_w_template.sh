6b8755e3ca2d89817a0f5535d9bb71a394ada1aa
tlepple <49572881+tlepple@users.noreply.github.com>
2020-09-22 12:32:52 -0500

    Update setup_w_template.sh

--- common/setup_w_template.sh
+++ common/setup_w_template.sh
@@ -368,12 +368,12 @@ pip install --upgrade pip cm_client
 ###########################################################################################################
 # update variables in the template
 ###########################################################################################################
-sed -i "s/YourHostname/`hostname -f`/g" $TEMPLATE
+sed -i "s/YourHostname/`hostname -f`/g" templates/$TEMPLATE
 sed -i "s/YourHostname/`hostname -f`/g" scripts/create_cluster.py
 # CDSW items
-sed -i "s/YourCDSWDomain/$PUBLIC_IP/g" $TEMPLATE
+sed -i "s/YourCDSWDomain/$PUBLIC_IP/g" templates/$TEMPLATE
 #sed -i "s/YourCDSWDomain/$PRIVATE_IP/g" $TEMPLATE
-sed -i "s/YourPrivateIP/$PRIVATE_IP/g" $TEMPLATE
+sed -i "s/YourPrivateIP/$PRIVATE_IP/g" templates/$TEMPLATE
 
 ###########################################################################################################
 # create the cluster with API
