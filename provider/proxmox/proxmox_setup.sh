c1fd0c58d95656bb837e2bef95dda137b7b787de
tlepple <49572881+tlepple@users.noreply.github.com>
2020-09-22 14:37:45 -0500

    Update proxmox_setup.sh

--- provider/proxmox/proxmox_setup.sh
+++ provider/proxmox/proxmox_setup.sh
@@ -5,11 +5,13 @@
 #########################################################
 PROVIDER=$1
 TEMPLATE=$2
+BLOCKDEVICE=$3
 
 echo "Begin install into ProxMox Instance from the proxmox_setup.sh script..."
 
 echo "Parameter PROVIDER -->" $PROVIDER
 echo "Parameter TEMPLATE -->" $TEMPLATE
+echo "Parameter BLOCKDEVICE -->" $BLOCKDEVICE
 #time issues for clock offset in aws	
 
 
@@ -17,4 +19,4 @@ echo "Parameter TEMPLATE -->" $TEMPLATE
 # install the common items from script 
 ###########################################################################################################
 echo "calling common_setup.sh here..."
-. $starting_dir/common/common_setup.sh $PROVIDER $TEMPLATE
+. $starting_dir/common/common_setup.sh $PROVIDER $TEMPLATE $BLOCKDEVICE
