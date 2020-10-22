8123deb0182295e4a875402090837892025808c1
tlepple <49572881+tlepple@users.noreply.github.com>
2020-10-22 14:08:55 -0500

    Update spark.iot.sh

--- common/component/efm_stuff/spark_stuff/spark.iot.sh
+++ common/component/efm_stuff/spark_stuff/spark.iot.sh
@@ -9,10 +9,10 @@ sed -i "s/YourCDSWDomain/cdsw.${PUBLIC_IP}.nip.io/" ~/spark.iot.py
 sed -i "s/YourAccessKey/${ACCESS_KEY}/" ~/spark.iot.py
 #wget  http://central.maven.org/maven2/org/apache/kudu/kudu-spark2_2.11/1.9.0/kudu-spark2_2.11-1.9.0.jar
 #wget https://repo1.maven.org/maven2/org/apache/kudu/kudu-spark2_2.11/1.9.0/kudu-spark2_2.11-1.9.0.jar
-#wget https://raw.githubusercontent.com/swordsmanliu/SparkStreamingHbase/master/lib/spark-core_2.11-1.5.2.logging.jar
+wget https://raw.githubusercontent.com/swordsmanliu/SparkStreamingHbase/master/lib/spark-core_2.11-1.5.2.logging.jar
 
 wget https://repository.cloudera.com/artifactory/cloudera-repos/org/apache/kudu/kudu-spark2_2.11/1.12.0.7.1.3.0-100/kudu-spark2_2.11-1.12.0.7.1.3.0-100.jar
-wget https://repository.cloudera.com/artifactory/cloudera-repos/org/apache/spark/spark-core_2.11/2.4.0.7.1.3.0-100/spark-core_2.11-2.4.0.7.1.3.0-100.jar
+#wget https://repository.cloudera.com/artifactory/cloudera-repos/org/apache/spark/spark-core_2.11/2.4.0.7.1.3.0-100/spark-core_2.11-2.4.0.7.1.3.0-100.jar
 
 rm -rf ~/.m2 ~/.ivy2/
 #spark-submit --master local[2] --jars kudu-spark2_2.11-1.9.0.jar,spark-core_2.11-1.5.2.logging.jar --packages org.apache.spark:spark-streaming-kafka_2.11:1.6.3 ~/spark.iot.py
