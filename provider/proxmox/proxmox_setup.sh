37026c74652c352b79242dfa3ba107f570fcf599
tlepple <49572881+tlepple@users.noreply.github.com>
2020-10-26 13:38:37 -0500

    Update proxmox_setup.sh

--- provider/proxmox/proxmox_setup.sh
+++ provider/proxmox/proxmox_setup.sh
@@ -28,6 +28,13 @@ sed -i "s/YourCDSWDomain/$PRIVATE_IP/g" $starting_dir/common/templates/$TEMPLATE
 #########################################################
 sed -i "s/YourCDSWDomain/cdsw.${PRIVATE_IP}.nip.io/" $starting_dir/common/component/nifi_templates/cdsw_rest_api.xml
 
+
+#########################################################
+# update the spark streaming code
+#########################################################
+
+#sed -i "s/YourCDSWDomain/cdsw.${PRIVATE_IP}.nip.io/" ~/spark.iot.py
+
 ###########################################################################################################
 # install the common items from script 
 ###########################################################################################################
