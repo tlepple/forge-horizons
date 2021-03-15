68ea8964d80c1b87d8cb43dda0fd78377efc42be
tlepple <49572881+tlepple@users.noreply.github.com>
2021-03-15 14:59:27 -0500

    Update setup_cm_only.sh

--- common/setup_cm_only.sh
+++ common/setup_cm_only.sh
@@ -48,7 +48,7 @@ BLOCKDEVICE=$2
 ###########################################################################################################
 CLDR_CM_LOCATION="@archive.cloudera.com/p/cm7"
 CLDR_MGR_BASEURL="https://$CLDR_REPO_USER:$CLDR_REPO_PASS$CLDR_CM_LOCATION"
-CLDR_MGR_VER_URL="$CLDR_MGR_BASEURL/7.0.3/redhat7/yum"
+CLDR_MGR_VER_URL="$CLDR_MGR_BASEURL/7.2.4/redhat7/yum"
 
 wget $CLDR_MGR_VER_URL/cloudera-manager-trial.repo -P /etc/yum.repos.d/
 
